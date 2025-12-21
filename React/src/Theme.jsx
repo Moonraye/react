7f8e092bbdd01a258f4c9e4cacb464edfdf701b1
Oleg Zavedia <olegzavedia0@gmail.com>
2025-12-21 15:20:04 +0200

    added theme meme

--- React/src/Theme.jsx
+++ React/src/Theme.jsx
@@ -1,11 +1,22 @@
 import { useState, useEffect } from "react";
 import themeLogo from "/night-mode.png";
 export default function themeSwitcher() {
+    const [count , setCount] = useState(0);
+    const [isDisabled, setIsDisabled] = useState(false);
     const [isDarkMode, setIsDarkMode] = useState(() => {
         const savedTheme = localStorage.getItem("theme");
         if (savedTheme) return savedTheme === "dark";
         return window.matchMedia("(prefers-color-scheme: dark)").matches;
     });
+useEffect(() => {
+    if (count === 10) alert("Listen, I'm tired of repainting the site! Let's stop? ^_^");
+    if (count === 20) alert("Okay, that's enough! Please stop clicking the button!");
+    if (count === 30) {
+        alert("Fine, goodbye! :( " );
+        setIsDisabled(true);
+    }
+}, [count]);
+
 useEffect(() => {
     const root = window.document.documentElement;
     if (isDarkMode) {
@@ -18,11 +29,13 @@ useEffect(() => {
 }, [isDarkMode]);
     
 const switchTheme = () => {
+    if (isDisabled) return;
     setIsDarkMode(!isDarkMode);
-}
+    setCount(count + 1);
 
+}
     return (
-        <button onClick={switchTheme}>
+        <button onClick={switchTheme} disabled={isDisabled}>
             <img src={themeLogo} alt="ThemeMod" className="w-15 h-15"/>
         </button>
     )
